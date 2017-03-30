a199c561537eb41924c2781118347606c87c6128
Brian11f <brianfinck@zumiez.com>
2017-03-30 11:06:48 -0700

    Added interval node once a day

---
+++ setInterval.js
@@ -0,0 +1,26 @@
+var exec = require('child_process').exec
+var path = require('path')
+var running = false
+
+var run = function(what, where) {
+  if (running === true) return
+  running = true
+
+  // by default, just run grunt
+  what = what || 'grunt phantomas'
+  // by default, run on grunt in the current working directory
+  where = path.resolve(where || path.join(process.cwd(), 'Gruntfile.coffee'))
+
+  what += ' --gruntfile=' + where
+  exec(what, { cwd: path.dirname(where) }, function(err, stdout, stderr) {
+    if (err || stderr) { /* log the error somewhere */ }
+    /* log the stdout if needed*/
+    console.log(stdout)
+    running = false
+  })
+}
+
+setInterval(function() {
+  run(/* set what to run, where to run */)
+  /* or even multiple gruntfiles and node projects */
+}, 24 * 60 * 60 * 1000) // once a day
